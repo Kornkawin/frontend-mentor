2a4fc3c21e003f1e665273f1375efec64ef9bd0a
kornkawin <k.bijayayothin@gmail.com>
2024-05-13 23:25:14 +0700

    [qr-code] finish project

--- qr-code-component/app/page.js
+++ qr-code-component/app/page.js
@@ -3,7 +3,7 @@ import QrCard from "@/components/QrCard";
 export default function Home() {
   return (
     <main className="bg-page min-h-screen flex flex-col items-center justify-center">
-      <div className="grid grid-cols-1">
+      <div className="grid grid-cols-1 max-w-sm">
         <QrCard/>
       </div>
     </main>
